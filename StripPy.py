6ad83e546770fc48404653d3c32f6bc6b143dee7
Thomas Millward Wright <thomas@ttrmw.co.uk>
2014-06-16 21:10:02 +0100

    update StripPy.py

--- StripPy.py
+++ StripPy.py
@@ -1,7 +1,3 @@
-import socket
-
-<<<<<<< HEAD
-
 class StrippyBot():
 
     def __init__(self, host, port, channel):
@@ -70,48 +66,3 @@ class StrippyBot():
 if __name__ == "__main__":
     bot = StrippyBot("irc.emerge-it.co.uk", 6667, "#dev")
     bot.connect()
-
-
-
-
-=======
-host = "irc.emerge-it.co.uk"
-port = 6667
-channel = "#dev"
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host, port))
-
-sock.send("NICK stripPy\n")
-sock.send("USER stripPy 0 * :test\n")
-
-sock.send("PRIVMSG nickserv :identify\n")
-
-registered = False
-
-
-def send_ch(msg, chan):
-    sock.send("PRIVMSG " + chan + " :" + msg + "\r\n")
-    print "PRIVMSG " + chan + " :" + msg
-
-
-while not registered:
-    if sock.recv(4096).find("001") != -1:
-        sock.send("JOIN " + channel + "\r\n")
-        registered = True
-
-connected = True
-while connected:
-    try:
-        mail = sock.recv(4096)
-    except socket.error as ex:
-        print ex.message
-        break
-
-    print mail
-    if mail.find("PING") != -1:
-        print "found ping, sending pong"
-        sock.send("PONG " + mail.split()[1] + "\r\n")
-        print "PONG " + mail.split()[1] + "\r\n"
-    if mail.find("StripPy") != -1:
-        send_ch("that's me!", channel)
->>>>>>> 77d3b7a3e8b29d40748a0f6b1b5efa161f2211fa
